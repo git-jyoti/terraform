f807406d6181358da66c36bd7661ac58750aeb6e
git-jyoti <122430744+git-jyoti@users.noreply.github.com>
2025-08-04 07:56:00 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "region" {
+    default  = "ap-south-1"
+}
+
+variable "accesskey" {
+    default = "AKIAXMMTWNBFO2INKP7G"
+}
+
+variable "secretkey" {
+    default = "AxK6M4pGn9sYRZ1mlfdX7iLN5dgQB0kE1n5zaNRw"
+}
+
+variable "imageid" {
+    default = "ami-0f918f7e67a3323f0"
+}
+
+variable "key" {
+    default = "test090"
+}
+
+variable "securitygroups" {
+    default = ["test090sg"]
+}
+
+variable "instancetype" {
+    default = "t2.micro"
+}
+
